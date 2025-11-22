897187175d2ce3656f081203389377799b48edbb
CyberCharlie-bh <mycoding005@gmail.com>
2025-11-22 09:02:29 +0000

    Create portscanner.py
    It is not a working module please dont try and use it

---
+++ Intermediate/portscanner.py
@@ -0,0 +1,21 @@
+# This does not contain threading method
+
+import socket
+
+HOST = input("Please enter the IP address:- ")
+PORT1 = int(input("Please enter the port start scan from:- "))
+PORT2 = int(input("Please enter the port end scan at:- "))
+print(f'Scanning {HOST} between {PORT1} and {PORT2}')
+
+for PORT in range(PORT1,(PORT2+1)):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(1)
+
+    Connect = client.connect_ex((HOST,PORT))
+
+    if Connect == 0:
+        print(f'{PORT} is open')
+
+    else:
+        pass
+    client.close()
